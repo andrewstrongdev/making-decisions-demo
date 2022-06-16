77d517f767063ee88e201175847522999f70b8d0
Andrew Strong <andrewstrongdev@gmail.com>
2022-06-16 12:54:41 -0600

    Initial commit

--- arrays.js
+++ arrays.js
@@ -2,3 +2,26 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = []
+let belt = []
+let bag = []
+
+backpack.push("lightsaber")
+backpack.push("robe","food")
+
+belt[0] = backpack.shift()
+
+backpack.splice(0,0,"fur-coat")
+
+bag.unshift("flint","blanket","toothbrush")
+
+backpack.push(bag.slice(0,3))
+
+// for(let i=0; i < bag.length; i++){
+//     console.log(bag[i])
+// }
+
+for(let i=0; i < 3; i++){
+console.log(backpack[i])    
+}
